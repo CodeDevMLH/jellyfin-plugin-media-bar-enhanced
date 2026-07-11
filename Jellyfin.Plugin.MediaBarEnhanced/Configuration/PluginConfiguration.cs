e5abe6faf943100353c3b36e72c7c6c4cde9ba0f
CodeDevMLH <145071728+CodeDevMLH@users.noreply.github.com>
2026-07-11 23:28:21 +0200

    Add new configuration options for local trailers and progress bar animation

--- Jellyfin.Plugin.MediaBarEnhanced/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.MediaBarEnhanced/Configuration/PluginConfiguration.cs
@@ -73,5 +73,7 @@ namespace Jellyfin.Plugin.MediaBarEnhanced.Configuration
         public string ProgressBarLocation { get; set; } = "Dots";
         public string CustomPlaylists { get; set; } = "[]";
         public string ExcludedLibraries { get; set; } = "";
+        public bool OnlyLocalTrailers { get; set; } = false;
+        public bool YoYoProgressBar { get; set; } = true;
     }
 }
